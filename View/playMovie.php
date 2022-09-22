b67a5b82d68bf67fc56733cb97ccc52c859616f8
Sandbagg45 <oliverdickens1990@gmail.com>
2022-09-22 18:14:34 +0100

    PlayMovies: clean up and sorted video player size

--- View/playMovie.php
+++ View/playMovie.php
@@ -12,45 +12,57 @@ Include '../Controller/getMovieByID.php';
 require '../Controller/StringManipulation.php';
 ?>
 <body>
-  <div class="container text-center mt-5 mb-3 pt-3">
-    <?php
-    echo "<h1 class='movieTitle'>".$movieArray->Title."</h1><br>";
-    ?>
-  </div>
-  <div class="playVideo">
-    <?php
-    echo "<video src='".$movieArray->Video_link."' type='video/mp4' autoplay controls>";
-      echo "<track src='".substr($movieArray->Video_link, 0, -4).".vtt' kind='subtitles' srclang='en' label='default' default>";
-      echo "<track src='".substr($movieArray->Video_link, 0, -4).".vtt' kind='subtitles' srclang='en' label='English'>";
-    echo "</video>"
-    ?>
-  </div>
-  <div class="container mt-5 mb-5 pt-3">
-    <div class="row">
-      <div class="videoPoster col-3">
-        <?php
-          echo "<img src='".$movieArray->Image_link."' onerror=this.src='Images/film.placeholder.poster.jpg'></a>"; // card image
-        ?>
-      </div>
-      <div class="videoDescription col-9">
-        <?php
-          $UKCertification = string_between_two_string($movieArray->Certification, 'United Kingdom:', '/');
-          echo "<div class='card-body'>";
-          echo "<h2 class='movieTitle'>".$movieArray->Title."";
-          echo "<h6 class='movieTitle text-muted'>".nl2br($movieArray->Description)."</h5>";
-          echo "<h6 class='movieTitle'><b>Year: </b>".$movieArray->Year."";
-          echo "<h6 class='movieTitle'><b>Genre: </b>".$movieArray->Genre."";
-          echo "<h6 class='movieTitle'><b>Rating: </b>".$movieArray->Rating."/10";
-          echo "<h6 class='movieTitle'><b>Certification: </b>".$UKCertification."";
-          echo "<h6 class='movieTitle'><b>Runtime: </b>".$movieArray->Runtime."";
-          echo "<h6 class='movieTitle'><b>Director: </b>".$movieArray->Director."";
-          echo "<h6 class='movieTitle'><b>Cast: </b><text class='show-read-more'>".$movieArray->Cast."</text>";
-          echo "<h6 class='movieTitle'><b>Quality: </b>".$movieArray->Quality."";
-          echo "</div>";
-        ?>
-      </div>
-    </div>
-  </div>
+    <main id="main" class="main">
+      <section class="section">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="card">
+              <div class="card-body">
+                <!-- Movie Description and Extras -->
+                <?php
+                echo "<h5 class='card-title'>".$movieArray->Title."</h5>";
+                $UKCertification = string_between_two_string($movieArray->Certification, 'United Kingdom:', '/');
+                echo "<div class='card-body'>";
+                // echo "<h2 class='movieTitle'>".$movieArray->Title."";
+                echo "<h5 class='movieTitle text-muted'>".nl2br($movieArray->Description)."</h5>";
+                echo "<h6 class='movieTitle'><b>Year: </b>".$movieArray->Year."";
+                echo "<h6 class='movieTitle'><b>Genre: </b>".$movieArray->Genre."";
+                echo "<h6 class='movieTitle'><b>Rating: </b>".$movieArray->Rating."/10";
+                echo "<h6 class='movieTitle'><b>Certification: </b>".$UKCertification."";
+                echo "<h6 class='movieTitle'><b>Runtime: </b>".$movieArray->Runtime."";
+                echo "<h6 class='movieTitle'><b>Director: </b>".$movieArray->Director."";
+                echo "<h6 class='movieTitle'><b>Cast: </b><text class='show-read-more'>".$movieArray->Cast."</text>";
+                echo "<h6 class='movieTitle'><b>Quality: </b>".$movieArray->Quality."";
+                echo "</div>";
+                ?>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="card">
+              <div class="card-body">
+                <!-- Movie Poster -->
+                <?php
+                echo "<img src='".$movieArray->Image_link."' class='card-img-bottom' style='width: 28rem; height: 32rem;'' onerror=this.src='Images/film.placeholder.poster.jpg'></a>";
+                ?>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="embed-responsive embed-responsive-16by9">
+
+            <?php
+            echo "<video src='".$movieArray->Video_link."' type='video/mp4' autoplay controls>";
+            echo "<track src='".substr($movieArray->Video_link, 0, -4).".vtt' kind='subtitles' srclang='en' label='default' default>";
+            echo "<track src='".substr($movieArray->Video_link, 0, -4).".vtt' kind='subtitles' srclang='en' label='English'>";
+            echo "</video>"
+            ?>
+          </div>
+        </div>
+      </section>
+    <main>
 </body>
 <?php
   include 'footer.php';
